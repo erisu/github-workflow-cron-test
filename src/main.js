2b19415abddefb7207847cbdec16b02cd8cf99bb
Erisu <ellis.bryan@gmail.com>
2020-08-21 16:38:45 +0900

    chore: fix workflow?

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
+const { mkdirSync } = require('fs');
+
 module.exports = () => {
-  console.log(process.argv)
+  console.log(process.argv);
+
+  // Making some directories... Mocking a clone from coho
+  mkdirSync('./cordova-cli');
+  mkdirSync('./cordova-lib');
+  mkdirSync('./cordova-common');
+  mkdirSync('./cordova-fetch');
+  mkdirSync('./cordova-serve');
+  mkdirSync('./cordova-create');
+  mkdirSync('./cordova-node-xcode');
+  mkdirSync('./cordova-android');
+  mkdirSync('./cordova-electron');
+  mkdirSync('./cordova-ios');
 }
